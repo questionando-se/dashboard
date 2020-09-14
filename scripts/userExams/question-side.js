4aae8d667024406cfc52a8e4308cc5f8fc20309c
EduardoJM <eduardo_y05@outlook.com>
2020-09-13 22:00:36 -0300

    updated userExams to better support with modals

--- scripts/userExams/question-side.js
+++ scripts/userExams/question-side.js
@@ -12,24 +12,104 @@ function createDropDown(info, infoHeading) {
     M.Dropdown.init(dropDownTrigger, {});
 }
 
+function getParentLi(element) {
+    if (element === null || element === undefined) {
+        return null;
+    }
+    if (element.nodeName === 'LI') {
+        return element;
+    } else {
+        return getParentLi(element.parentElement);
+    }
+}
+
 function onSaveQuestionModalOpen() {
     var exams = getExamCollection();
     if (!exams) {
         exams = [];
     }
+    var html = '<ul id="exams-collection" class="collection">';
     for (var i = 0; i < exams.length; i += 1) {
-
+        html += '<li data-id="' + exams[i].id + '" class="collection-item">' + exams[i].name + '<a class="secondary-content modal-close"><i class="material-icons">send</i></a></li>';
+    }
+    html += '</ul>';
+    var container = document.getElementById('add-question-exam-list');
+    container.innerHTML = html;
+    var examItems = container.querySelectorAll('li a.secondary-content');
+    for (var i = 0; i < examItems.length; i += 1) {
+        examItems[i].addEventListener('click', function(e) {
+            var target = e.target;
+            var li = getParentLi(target);
+            if (li === null) {
+                return;
+            }
+            var id = li.getAttribute('data-id');
+            if (id === null || id === undefined) {
+                return;
+            }
+            var exams = getExamCollection();
+            var splitedPathName = location.pathname.split('/');
+            var fileName = splitedPathName[splitedPathName.length - 1];
+            var fileId = fileName.replace('.html', '');
+            for (var j = 0; j < exams.length; j += i) {
+                if (exams[j].id === id) {
+                    if (!exams[j].questions.includes(fileId)) {
+                        exams[j].questions.push(fileId);
+                    }
+                }
+            }
+            setExamCollection(exams);
+        });
     }
 }
 
+function createAddNewExamModal() {
+    var innerHTML = 
+        '<div class="modal-content">' +
+        '   <h4>Adicionar Nova Prova</h4>' +
+        '   <div>' +
+        '       <div class="row">' +
+        '           <div class="input-field col s12">' +
+        '               <input id="new-exam-name" type="text" />' +
+        '               <label for="new-exam-name" class="">Nome da Prova</label>' +
+        '           </div>' +
+        '       </div>' +
+        '   </div>' +
+        '</div>' +
+        '<div class="modal-footer">' +
+        '   <a class="modal-close waves-effect waves-green btn-flat">Cancelar</a>' +
+        '   <a id="new-exam-add-btn" class="modal-close waves-effect waves-green btn-flat">Adicionar</a>' +
+        '</div>';
+    var modal = document.createElement('div');
+    modal.setAttribute('id', 'add-exam-to-list');
+    modal.className = 'modal bottom-sheet';
+    modal.innerHTML = innerHTML;
+    document.body.appendChild(modal);
+    var createExamButton = modal.querySelector('#new-exam-add-btn')
+    createExamButton.addEventListener('click', function() {
+        var nameInput = modal.querySelector('#new-exam-name');
+        var name = nameInput.value;
+        var exam = createExam(name);
+        var splitedPathName = location.pathname.split('/');
+        var fileName = splitedPathName[splitedPathName.length - 1];
+        exam.questions.push(fileName.replace('.html', ''));
+        var examsCollection = getExamCollection();
+        examsCollection.push(exam);
+        setExamCollection(examsCollection);
+    });
+    M.Modal.init(modal);
+}
+
 function createModal() {
+    createAddNewExamModal();
     var innerHTML = 
         '<div class="modal-content">' +
         '   <h4>Adicionar Questão</h4>' +
         '   <div id="add-question-exam-list"></div>' +
         '</div>' +
         '<div class="modal-footer">' +
-        '   <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>' +
+        '   <a href="#add-exam-to-list" class="modal-close modal-trigger waves-effect waves-green btn-flat">Adicionar Nova Prova</a>' +
+        // '   <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>' +
         '</div>';
     var modal = document.createElement('div');
     modal.setAttribute('id', 'add-question-to-exam-modal');
@@ -44,9 +124,9 @@ function createModal() {
 document.addEventListener('DOMContentLoaded', function(){
     var info = document.querySelector('main .content .container .info');
     var infoHeading = info.querySelector('h4');
-    if (!info || !infoHeading) {
-        return;
+    if (info !== null && info !== undefined 
+        && infoHeading !== null && infoHeading !== undefined) {
+        createDropDown(info, infoHeading);
+        createModal();
     }
-    createDropDown(info, infoHeading);
-    createModal();
 });
